37f7326e5085fdd0f6714759735d5297c0c519f6
Anika-Tazin-Pranty <anika.tazin.pranty@g.bracu.ac.bd>
2024-12-14 14:59:39 +0600

    Worked on product addition

--- main.py
+++ main.py
@@ -4,8 +4,4 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-<<<<<<< Updated upstream
     app.run(debug = True, port=5001) # default port = 5001
-=======
-    app.run(debug = True, port=5001) # default port = 5001
->>>>>>> Stashed changes
